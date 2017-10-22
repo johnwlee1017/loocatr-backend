dc9456ba74d0e3d71be0de3a41b3d084bc014577
yomoda07 <kanji.yy@gmail.com>
2017-10-22 14:45:44 -0700

    Fix error happend when seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 i = 0
-5.times do
+20.times do
   response = HTTParty.get("https://api.yelp.com/v3/businesses/search?location=sanfrancisco&attributes=gender_neutral_restrooms&offset=#{i}", headers: {Authorization: "Bearer dBIo5uV7Nn-Dl9CV2AzSjZ6SA3DE0KneUI_wwdOtRN8P7NnoDcbjKeSMBrKTg8b8nsQO0brYUl-v8n3DSAEtNu1sqWxcbErMekhbtXTMpo-e6CbFSLqXwDte5orrWXYx"} )
 
   businesses = JSON.parse(response.body)
@@ -15,8 +15,10 @@ i = 0
     bathroom = Bathroom.find_by(id: id[:db_id])
     response = HTTParty.get("https://api.yelp.com/v3/businesses/#{URI.encode(id[:yelp_id])}", headers: {Authorization: "Bearer dBIo5uV7Nn-Dl9CV2AzSjZ6SA3DE0KneUI_wwdOtRN8P7NnoDcbjKeSMBrKTg8b8nsQO0brYUl-v8n3DSAEtNu1sqWxcbErMekhbtXTMpo-e6CbFSLqXwDte5orrWXYx"} )
     business =  JSON.parse(response.body)
-    business["hours"][0]["open"].each do |open|
-      bathroom.time_frames.create(day: open["day"], is_overnight: open["is_overnight"], start: open["start"].to_s, close: open["end"].to_s)
+    if business["hours"]
+      business["hours"][0]["open"].each do |open|
+        bathroom.time_frames.create(day: open["day"], is_overnight: open["is_overnight"], start: open["start"].to_s, close: open["end"].to_s)
+      end
     end
   end
   i += 20
