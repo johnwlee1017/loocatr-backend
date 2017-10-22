880b64092cf7779882556f4cb2738c117af051d7
yomoda07 <kanji.yy@gmail.com>
2017-10-21 19:31:09 -0700

    Fix duplicated seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-
-5.times do |i|
+i = 0
+5.times do
   response = HTTParty.get("https://api.yelp.com/v3/businesses/search?location=sanfrancisco&attributes=gender_neutral_restrooms&offset=#{i}", headers: {Authorization: "Bearer dBIo5uV7Nn-Dl9CV2AzSjZ6SA3DE0KneUI_wwdOtRN8P7NnoDcbjKeSMBrKTg8b8nsQO0brYUl-v8n3DSAEtNu1sqWxcbErMekhbtXTMpo-e6CbFSLqXwDte5orrWXYx"} )
 
   businesses = JSON.parse(response.body)
@@ -7,19 +7,17 @@
   ids = []
 
   businesses["businesses"].each do |business|
-    @bathroom = Bathroom.find_by(latitude: business["coordinates"]["latitude"], longitude: business["coordinates"]["longitude"])
-    if !@bathroom
-      bathroom = Bathroom.create(location_name: business["name"], latitude: business["coordinates"]["latitude"], longitude: business["coordinates"]["longitude"])
-      ids << { yelp_id: business["id"], db_id: bathroom.id }
-    end
+    bathroom = Bathroom.create(location_name: business["name"], latitude: business["coordinates"]["latitude"], longitude: business["coordinates"]["longitude"])
+    ids << { yelp_id: business["id"], db_id: bathroom.id }
   end
 
   ids.each do |id|
     bathroom = Bathroom.find_by(id: id[:db_id])
-    response = HTTParty.get("https://api.yelp.com/v3/businesses/#{id[:yelp_id]}", headers: {Authorization: "Bearer dBIo5uV7Nn-Dl9CV2AzSjZ6SA3DE0KneUI_wwdOtRN8P7NnoDcbjKeSMBrKTg8b8nsQO0brYUl-v8n3DSAEtNu1sqWxcbErMekhbtXTMpo-e6CbFSLqXwDte5orrWXYx"} )
+    response = HTTParty.get("https://api.yelp.com/v3/businesses/#{URI.encode(id[:yelp_id])}", headers: {Authorization: "Bearer dBIo5uV7Nn-Dl9CV2AzSjZ6SA3DE0KneUI_wwdOtRN8P7NnoDcbjKeSMBrKTg8b8nsQO0brYUl-v8n3DSAEtNu1sqWxcbErMekhbtXTMpo-e6CbFSLqXwDte5orrWXYx"} )
     business =  JSON.parse(response.body)
     business["hours"][0]["open"].each do |open|
       bathroom.time_frames.create(day: open["day"], is_overnight: open["is_overnight"], start: open["start"].to_s, close: open["end"].to_s)
     end
   end
+  i += 20
 end
