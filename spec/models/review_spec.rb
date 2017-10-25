14175486d69589e0789fa5ded5427f6228641632
yomoda07 <kanji.yy@gmail.com>
2017-10-25 14:21:39 -0700

    Fix associations between user and reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,5 +4,4 @@ RSpec.describe Review, type: :model do
   it { should validate_presence_of(:ratings) }
   it { should validate_presence_of(:description) }
   it { should belong_to(:bathroom) }
-  it { should belong_to(:user) }
 end
