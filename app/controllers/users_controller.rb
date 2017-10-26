8c9bbb9b46a159a6f7cafc1ebbb4ef41aaf49601
yomoda07 <kanji.yy@gmail.com>
2017-10-26 15:45:57 -0700

    Fix create method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     user_id = params[:user_id]
     @bathrooms = Bathroom.where(user_id: user_id)
     @reviews = Review.where(user_id: user_id)
-    @histories = UseHistory.where(user_id: user_id)
+    @histories = UseHistory.where(user_id: user_id).map { |history| history.bathroom }
     json_response({bathrooms: @bathrooms, reviews: @reviews, histories: @histories})
   end
 end
