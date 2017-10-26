f3ebf1fb7d962a2fe23795e6906419f7348e801a
yomoda07 <kanji.yy@gmail.com>
2017-10-26 14:07:59 -0700

    Fix route and Histries controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index]
   resources :bathrooms do
     resources :reviews
     resources :time_frames
-    resources :images, only: [:index, :craete]
-    resources :use_histories, only: [:craete]
+    resources :images, only: [:index, :create]
+    resources :use_histories, only: [:create]
   end
 end
